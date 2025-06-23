d112c58a2e5c44d2bd864e2fa7169788bb5b9f22
nourhenta <nour.hentati@medtech.tn>
2025-06-23 10:22:49 +0100

    Update target framework to .NET 8.0 and clean old artifacts

--- obj/Debug/net8.0/HelloApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed72baa3a22d85659263ac5e792f79c66aa5b575")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
